e7d211fba9259cac6c13def7205010a18f564bc8
ViniciusCardoso19 <vinisilvacardoso10@gmail.com>
2026-05-28 21:51:47 -0300

    Add files via upload

---
+++ trabalhoingles/quiz.js
@@ -0,0 +1,12 @@
+let pergunta = []
+
+console.log("=== QUIZ GAME ===");
+    console.log("Choose difficulty:");
+    console.log("1 - Easy");
+    console.log("2 - Medium");
+    console.log("3 - Hard");
+
+    letLevelChoice = readline.question("Enter(1,2,3): ");
+
+    
+
